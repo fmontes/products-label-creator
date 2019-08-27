a77e29a8a2d3daec4c329214f5581255a5a47677
Freddy Montes <freddymontes@gmail.com>
2019-08-27 16:03:51 -0600

    Render labels basic

--- ts/main.ts
+++ ts/main.ts
@@ -1,6 +1,7 @@
 import csv from 'csvtojson';
 
 const input: HTMLInputElement = document.querySelector('input[type="file"]');
+const main: HTMLElement = document.querySelector('main');
 
 input.addEventListener('change', handleChange);
 
@@ -8,15 +9,31 @@ function handleChange({ target }: HTMLInputEvent) {
     const file = target.files[0];
 
     const reader = new FileReader();
-    reader.onload = (e: ProgressEvent) => {
-        csv()
+    reader.onload = async (e: ProgressEvent) => {
+        const labels: LabelInformation[] = await csv()
             .fromString(reader.result as string)
-            .then((result: LabelInformation[]) => {
-                console.log(result);
-            });
+            .then((result: LabelInformation[]) => result);
+
+        main.innerHTML = labels.map(getLabelHtml).join('');
     };
     reader.readAsText(file);
 }
+
+function getLabelHtml({ name, barcode, price }: LabelInformation): string {
+    return `
+        <article>
+            <span>${barcode}</span>
+            <p class="name">${name}</p>
+            <p class="price">${formatter.format(parseInt(price, 10))}</p>
+        </article>
+    `;
+}
+
+const formatter = new Intl.NumberFormat('es-CR', {
+    style: 'currency',
+    currency: 'CRC',
+    minimumFractionDigits: 2
+});
 
 // MODELS
 interface HTMLInputEvent extends Event {
