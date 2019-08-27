bebf2fa7c0d9550b476a9274ac2f8efdc1ad6e0f
Freddy Montes <freddymontes@gmail.com>
2019-08-27 15:44:40 -0600

    Read the csv file

--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,30 @@
-console.log('hello');
+import csv from 'csvtojson';
+
+const input: HTMLInputElement = document.querySelector('input[type="file"]');
+
+input.addEventListener('change', handleChange);
+
+function handleChange({ target }: HTMLInputEvent) {
+    const file = target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (e: ProgressEvent) => {
+        csv()
+            .fromString(reader.result as string)
+            .then((result: LabelInformation[]) => {
+                console.log(result);
+            });
+    };
+    reader.readAsText(file);
+}
+
+// MODELS
+interface HTMLInputEvent extends Event {
+    target: HTMLInputElement & EventTarget;
+}
+
+interface LabelInformation {
+    name: string;
+    barcode: string;
+    price: string;
+}
